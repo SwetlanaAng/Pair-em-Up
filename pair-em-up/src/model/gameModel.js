a5100e0a1c2894eabb8ea1f25f2d9f5ad33bfac2
SwetlanaAng <93288183+SwetlanaAng@users.noreply.github.com>
2025-11-12 14:54:49 +0100

    fix: correct getting game field

--- pair-em-up/src/model/gameModel.js
+++ pair-em-up/src/model/gameModel.js
@@ -1,10 +1,6 @@
 export default class GameModel {
   constructor() {
-    this.initialGameField = [
-        [1, 2, 3, 4, 5, 6, 7, 8, 9],
-        [1, 1, 1, 2, 1, 3, 1, 4, 1],
-        [5, 1, 6, 1, 7, 1, 8, 1, 9],
-      ];
+    this.initialGameField = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16, 17, 18, 19];
     this.gameField = [
       [1, 2, 3, 4, 5, 6, 7, 8, 9],
       [1, 1, 1, 2, 1, 3, 1, 4, 1],
@@ -22,7 +18,6 @@ export default class GameModel {
     } else if(this.gameMode === 'random'){
       return this.getRandomGameField();
     }
-    return this.gameField;
   }
 
   getRowsCount() {
@@ -32,9 +27,33 @@ export default class GameModel {
   setGameMode(gameMode){
     this.gameMode = gameMode;
   }
-  getClassicGameField(){
-    this.gameField = structuredClone(this.initialGameField);
+
+  getGameFieldFromArray(array){
+    this.gameField = [];
+    const interimArray = [];
+    let chankArray = [];
+    array.forEach((item)=>{
+        if(item/10>1){
+            interimArray.push(1);
+            interimArray.push(item%10);
+        }else {
+            interimArray.push(item);
+        } 
+        return item;
+    })
+    for(let i = 0; i < interimArray.length; i++){
+        chankArray = interimArray.slice(i, i + 9);
+        if(chankArray.length<9){
+            chankArray.push(...Array(9 - chankArray.length).fill(0));
+        }
+        this.gameField.push(chankArray);
+        i = i+8;
+    }
     return this.gameField;
+  }
+  getClassicGameField(){
+    
+    return this.getGameFieldFromArray(this.initialGameField)
   }
   getChaoticGameField(){
     this.gameField = [];
@@ -44,10 +63,8 @@ export default class GameModel {
     return this.gameField;
   }
   getRandomGameField(){
-    this.gameField = structuredClone(this.initialGameField);
-    this.gameField = this.gameField.sort(() => Math.random() - 0.5); // тут баг с рандомом, рандом действует на строки а не на элементы в строках
-
-    return this.gameField;
+    let interimArray = Array.from(this.initialGameField);
+    return this.getGameFieldFromArray(interimArray.sort(() => Math.random() - 0.5))
   }
 
   getLeftNumbers(){
